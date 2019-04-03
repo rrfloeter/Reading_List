bcd8d041988f216c9345a3c46cb8e5763d271d88
rfloeter <rainer.r.floeter@gmail.com>
2019-04-03 18:54:26 -0400

    update

---
+++ Solved/client/build/precache-manifest.2dbdf05fea8d743026b50ae7dd89f24d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "98aecd4359e131683806",
+    "url": "/static/js/main.1435e4f9.chunk.js"
+  },
+  {
+    "revision": "19f0dc92cc37bea57c9d",
+    "url": "/static/js/2.4d4d1d51.chunk.js"
+  },
+  {
+    "revision": "98aecd4359e131683806",
+    "url": "/static/css/main.a40e68d1.chunk.css"
+  },
+  {
+    "revision": "dcca0439e11363fbcf4a4bcdd55c10ab",
+    "url": "/index.html"
+  }
+];
